e5cdb795f8c574492bb5e428752e090cf27dc07d
dluniewska <luniewska.d@gmail.com>
2022-06-10 14:42:09 +0200

    tested homepage and login, created classes for pages

---
+++ NUnitSelenium/Pages/HomePage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitSelenium.Pages
+{
+    internal class HomePage
+    {
+        private IWebDriver WebDriver { get; }
+
+        public HomePage(IWebDriver webDriver)
+        {
+            WebDriver = webDriver;
+        }
+
+        public IWebElement LoginElement  => WebDriver.FindElement(By.XPath("//a[contains(., 'Zaloguj się')]"));
+
+        public IWebElement HelloDescription => WebDriver.FindElement(By.XPath("//div[contains(., 'Zdrowe Włosy jest to baza danych zawierająca kosmetyki do pielęgnacji włosów. W opisie umieszczamy tylko składniki. Jeśli jesteś tu nowa ściągnij PDF z instrukcją jak dbać o włosy.')]"));
+
+        public void ClickLogin()
+        {
+            LoginElement.Click();
+        }
+
+        public bool IfHelloDesriptionExists() => HelloDescription.Displayed;
+    }
+}
---
+++ NUnitSelenium/Tests/zdrowe-wlosy-pages.cs
@@ -0,0 +1,50 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnitSelenium.Tests
+{
+    public class Tests
+    {
+        string url = "https://zdrowe-wlosy.netlify.app/";
+
+        [SetUp]
+        public void Setup()
+        {
+            ////Open browser
+            //IWebDriver webDriver = new ChromeDriver();
+
+            ////Navigate to site
+            //webDriver.Navigate().GoToUrl(url);
+        }
+
+        [Test]
+        public void Login()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl(url);
+            IWebElement Description = webDriver.FindElement(By.XPath("//div[contains(., 'Zdrowe W³osy jest to baza danych zawieraj¹ca kosmetyki do pielêgnacji w³osów. W opisie umieszczamy tylko sk³adniki. Jeœli jesteœ tu nowa œci¹gnij PDF z instrukcj¹ jak dbaæ o w³osy.')]"));
+            Assert.That(Description.Displayed, Is.True);
+
+            ////Open browser
+            //IWebDriver webDriver = new ChromeDriver();
+            ////Navigate to site
+            //webDriver.Navigate().GoToUrl(url);
+            ////Identify login
+            ////IWebElement login = webDriver.FindElement(By.XPath("//div[@id='root']/div/header/div[2]/a/span"));
+            //IWebElement login = webDriver.FindElement(By.XPath("//a[contains(., 'Zaloguj siê')]"));
+            //login.Click();
+            //var txtEmail = webDriver.FindElement(By.Name("email"));
+            //var txtPassword = webDriver.FindElement(By.Name("password"));
+            ////Assertion
+            //Assert.That(txtEmail.Displayed, Is.True);
+            //Assert.That(txtPassword.Displayed, Is.True);
+
+            //txtEmail.SendKeys("admin@admin");
+            //txtPassword.SendKeys("admin");
+            //IWebElement signinbtn = webDriver.FindElement(By.XPath("//button[contains(., 'Sign In')]"));
+            //signinbtn.Submit();
+            ////webDriver.FindElement(By.XPath("//h6[contains(., 'Dodaj Kosmetyk')]"));
+
+        }
+    }
+}
