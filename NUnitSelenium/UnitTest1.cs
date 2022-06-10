f3e90eba7bf221d34d8ce97be68ca4b3be5d00f5
dluniewska <luniewska.d@gmail.com>
2022-06-10 10:27:57 +0200

    Started Selenium NUnit Tests project

---
+++ NUnitSelenium/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NUnitSelenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
