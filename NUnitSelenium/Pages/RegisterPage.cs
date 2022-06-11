5b4546bf0d9a3171e307a092582e118e8595cf20
dluniewska <luniewska.d@gmail.com>
2022-06-11 19:38:23 +0200

    added register page files correctly

---
+++ NUnitSelenium/Pages/RegisterPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitSelenium.Pages
+{
+    internal class RegisterPage
+    {
+        private IWebDriver WebDriver { get; }
+
+        public RegisterPage(IWebDriver webDriver)
+        {
+            WebDriver = webDriver;
+        }
+    }
+}
---
+++ NUnitSelenium/Tests/RegisterPageTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnitSelenium.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitSelenium.Tests
+{
+    internal class RegisterPageTests
+    {
+        string url = "https://zdrowe-wlosy.netlify.app/";
+
+        //Browser driver
+        IWebDriver webDriver = new ChromeDriver();
+        HomePage homepage;
+        SignInPage signInPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Navigate to site
+            webDriver.Navigate().GoToUrl(url);
+            homepage = new HomePage(webDriver);
+            signInPage = new SignInPage(webDriver);
+            homepage.ClickLogin();
+            signInPage.ClickRegister();
+        }
+
+        [TearDown]
+        public void TearDown() => webDriver.Quit();
+    }
+}
