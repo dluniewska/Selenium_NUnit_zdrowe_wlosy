7f492a7ecf28949f9a3be9bc4a568d3ec1b6f62f
dluniewska <luniewska.d@gmail.com>
2022-06-12 21:37:55 +0200

    Created classes for loggedin page and tests, created test for adding new post

---
+++ NUnitSelenium/Pages/LoggedInPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitSelenium.Pages
+{
+    internal class LoggedInPage
+    {
+        private IWebDriver WebDriver { get; }
+
+        public IWebElement TitleInput => WebDriver.FindElement(By.Name("title"));
+        public IWebElement INCIInput => WebDriver.FindElement(By.Name("message"));
+        public IWebElement TagsInput => WebDriver.FindElement(By.Name("tags"));
+        public IWebElement SaveButton => WebDriver.FindElement(By.XPath("//button[contains(., 'ZAPISZ')]"));
+
+
+        public LoggedInPage(IWebDriver webDriver)
+        {
+            WebDriver = webDriver;
+        }
+
+        public void CreateNewPost(string name, string inci, string tags)
+        {
+            TitleInput.SendKeys(name);
+            INCIInput.SendKeys(inci);
+            TagsInput.SendKeys(tags);
+            SaveButton.Submit();
+        }
+    }
+}
---
+++ NUnitSelenium/Tests/LogedInTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnitSelenium.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitSelenium.Tests
+{
+    internal class LogedInTests
+    {
+        string url = "https://zdrowe-wlosy.netlify.app/";
+
+        //Browser driver
+        IWebDriver webDriver = new ChromeDriver();
+        HomePage homepage;
+        SignInPage signInPage;
+        LoggedInPage loggedInPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Navigate to site
+            webDriver.Navigate().GoToUrl(url);
+            homepage = new HomePage(webDriver);
+            signInPage = new SignInPage(webDriver);
+            loggedInPage = new LoggedInPage(webDriver);
+            homepage.ClickLogin();
+            signInPage.SignIn("admin@admin", "admin");
+        }
+
+        [Test]
+        public void CreateNewItem()
+        {
+            signInPage.IsLoggedSuccesfully();
+            loggedInPage.CreateNewPost("SeleniumTestPost", "SeleniumTestPostINCI", "SeleniumTestPostTag");
+            homepage.AwaitedCardWithTitleDisplayed("SeleniumTestPost");
+            Assert.That(homepage.IsCardWithTitleDisplayed("SeleniumTestPost"), Is.True);
+        }
+
+        [TearDown]
+        public void TearDown() => webDriver.Quit();
+    }
+}
